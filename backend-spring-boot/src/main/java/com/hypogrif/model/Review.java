2661a4d30ffa353596f8fe94b76c288591cd341b
Renato Ponce Barrera <rponce@Renatos-MacBook-Pro.local>
2024-04-06 20:32:59 -0500

    feature-entities-creation

---
+++ backend-spring-boot/src/main/java/com/hypogrif/model/Review.java
@@ -0,0 +1,32 @@
+package com.hypogrif.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User customer;
+
+    @ManyToOne
+    @JsonIgnore
+    private Restaurant restaurant;
+
+    private String message;
+
+    private double rating;
+
+    private LocalDateTime createdAt;
+}
